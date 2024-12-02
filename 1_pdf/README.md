4c363fa5f6fbc33bcf078944ba5cae4f0dcc48d7
matheus896 <matheus.ps23@hotmail.com>
2024-12-02 15:55:20 -0300

    Add files via upload

---
+++ 1_pdf/README.md
@@ -0,0 +1,68 @@
+# Um Sistema de Assistente Automático
+Este script Python utiliza a biblioteca crewai para criar uma estrutura de assistente automático (Crew) que busca e responde perguntas em um PDF. O sistema inclui dois agentes distintos: um agente de pesquisa para extrair dados do PDF e um agente de escrita profissional para escrever emails com base nas informações extraídas.
+
+# Pré-requisitos
+Python: Certifique-se de ter o Python instalado em sua máquina. A versão recomendada é a 3.12
+
+- Bibliotecas necessárias:
+
+crewai
+crewai_tools
+PyPDF2 (opcional, para uso com o PDFSearchTool)
+pydotenv
+
+- Para instalar as bibliotecas necessárias, execute:
+
+pip install crewai crewai_tools  
+pip install python-dotenv
+pip install PyPDF2 #opcional
+
+# Como Usar
+
+- Configurar o Ambiente:
+
+Certifique-se de que o arquivo .env esteja configurado com as variáveis necessárias, se houver algum.
+
+- Execute:
+
+venv\Scripts\activate # Para Windows
+source venv/bin/activate # Para Linux/Mac
+
+- Executar o Script:
+
+python 1_crew.py
+
+- Interagir com o Sistema:
+
+Você será solicitado a informar qual seção do relatório de inspeção deseja gerar uma ordem de trabalho.
+O sistema então buscará e responderá às perguntas fornecidas.
+
+# Estrutura do Sistema
+
+- Agentes:
+
+Research Agent: Responsável por pesquisar e extrair informações do PDF, garantindo precisão e rapididade nas buscas.
+Professional Writer Agent: Conhecido por suas habilidades de escrita profissional para criar emails a partir dos dados extraídos pelo Research Agent.
+- Tarefas:
+
+Answer Customer Question Task:
+
+Busca e responde perguntas do relatório de inspeção com base no conteúdo do PDF.
+
+Write Email Task:
+
+Escreve um email para um contratante, detalhando os problemas encontrados na seção especificada pelo usuário e solicitando uma ordem de trabalho ou plano para corrigi-los.
+O email é assinado com informações pessoais como nome e empresa.
+
+- Cronograma
+
+O sistema utiliza um cronograma sequencial (Process.sequential), onde as tarefas são executadas em ordem. Isso garante que todas as etapas necessárias estejam concluídas antes de avançar para a próxima.
+
+# Considerações
+O PDF precisa estar localizado no mesmo diretório que o script.
+Se você estiver usando um ambiente diferente (por exemplo, um servidor ou Docker), certifique-se de adaptar as configurações de ambiente corretamente.
+Este sistema foi projetado para uso em cenários específicos e pode necessitar adaptação para outros tipos de aplicativos.
+
+# Licença
+Este software é distribuído sob a licença MIT. Consulte o arquivo LICENSE para obter mais detalhes.
+
---
+++ 1_pdf/rag-pdf.py
@@ -0,0 +1,115 @@
+from crewai import Agent, Crew, Process, Task, LLM
+from crewai_tools import PDFSearchTool
+from dotenv import load_dotenv
+from langchain_ollama import OllamaEmbeddings
+
+embeddings = OllamaEmbeddings(model="nomic-embed-text:latest", base_url="http://localhost:11434")
+
+
+load_dotenv()
+
+llm = LLM (
+    model = "cerebras/llama3.1-70b",
+    temperature=0.1
+    )
+
+# --- Tools ---
+pdf_search_tool = PDFSearchTool(
+    pdf="./example_home_inspection.pdf",
+    config=dict(
+        embedder=dict(provider="ollama", config=dict(model="nomic-embed-text:latest")))
+)
+
+# --- Agents ---
+research_agent = Agent(
+    role="Agente de Pesquisa",
+    goal="Pesquisar no PDF para encontrar respostas relevantes",
+    allow_delegation=False,
+    verbose=True,
+    backstory=(
+        """
+        O agente de pesquisa é especialista em buscar e extrair dados de documentos,
+        garantindo respostas precisas e rápidas.
+        """
+    ),
+    tools=[pdf_search_tool],
+    llm = llm
+)
+
+professional_writer_agent = Agent(
+    role="Escritor Profissional",
+    goal="Escrever e-mails profissionais com base nas descobertas do agente de pesquisa",
+    allow_delegation=False,
+    verbose=True,
+    backstory=(
+        """
+        O agente escritor profissional possui excelentes habilidades de escrita e é capaz de
+        criar e-mails claros e concisos com base nas informações fornecidas.
+        """
+    ),
+    tools=[],
+    llm = llm
+)
+
+# --- Tasks ---
+answer_customer_question_task = Task(
+    description=(
+        """
+        Responda às perguntas do cliente com base no PDF da inspeção domiciliar.
+        O agente de pesquisa pesquisará no PDF para encontrar as respostas relevantes.
+        Sua resposta final DEVE ser clara e precisa, com base no conteúdo do PDF da inspeção domiciliar.
+
+        Aqui está a pergunta do cliente:
+        {customer_question}
+        """
+    ),
+    expected_output="""
+        Forneça respostas claras e precisas às perguntas do cliente com base no conteúdo do PDF da inspeção domiciliar.
+        """,
+    tools=[pdf_search_tool],
+    agent=research_agent,
+)
+
+write_email_task = Task(
+    description=(
+        """
+        Escreva um e-mail profissional para um empreiteiro com base nas descobertas do agente de pesquisa.
+        O e-mail deve declarar claramente os problemas encontrados na seção especificada do relatório e solicitar
+        um orçamento ou plano de ação para corrigir esses problemas.
+
+        Certifique-se de que o e-mail seja assinado com os seguintes detalhes:
+
+        Atenciosamente,
+
+        Brandon Hancock,
+        Hancock Realty
+        """
+    ),
+    expected_output="""
+        Escreva um e-mail claro e conciso que possa ser enviado a um empreiteiro para abordar os problemas encontrados
+        no relatório de inspeção domiciliar.
+        """,
+    tools=[],
+    agent=professional_writer_agent,
+)
+
+
+# --- Crew ---
+crew = Crew(
+    tasks=[answer_customer_question_task, write_email_task],
+    agents=[research_agent, professional_writer_agent],
+    process=Process.sequential,
+)
+
+customer_question = input(
+    "Para qual seção do relatório você gostaria de gerar uma ordem de serviço?\n"
+)
+result = crew.kickoff(inputs={"customer_question": customer_question})
+print(result)
+
+# venv\Scripts\activate
+# python 2_crew_custom_model_and_embeddings.py
+"""#     config=dict(
+        embedder=dict(llm=dict(provider="google", config=dict(model="gemini-1.5-flash")),
+                      provider="ollama", config=dict(model="nomic-embed-text:latest")),
+),  """
